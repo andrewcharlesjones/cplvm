b1b973d5cfa9dc2edd3f76878f45fb70f77c06bc
Andy Jones <ajones788@gmail.com>
2021-07-04 09:17:24 -0400

    new version

--- setup.py
+++ setup.py
@@ -2,10 +2,10 @@ from setuptools import setup, find_packages
 
 setup(
     name="cplvm",
-    version="0.1",
+    version="0.2",
     description="Contrastive Poisson latent variable model",
     url="https://github.com/andrewcharlesjones/cplvm",
-    download_url="https://github.com/andrewcharlesjones/cplvm/archive/refs/tags/v_01.tar.gz",
+    download_url="https://github.com/andrewcharlesjones/cplvm/archive/refs/tags/CPLVM_v02.tar.gz",
     author="Andy Jones",
     author_email="ajones788@gmail.com",
     packages=find_packages(),
